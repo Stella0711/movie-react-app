fc29790407e4e72f3a953ac7e438facb24747d1d
Stella0711 <crystal.quigley@gmail.com>
2026-08-24 23:07:25 -0500

    Complete main setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <main>
+    <div />
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  </main>
+  
+  )
+}
+
+export default App;
